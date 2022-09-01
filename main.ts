9251a085ef45585ef8e9fa6c6e6d07b5f67bb2d9
Musa Barighzaai <musa@circleci.com>
2022-09-01 13:55:31 -0400

    update config

--- main.ts
+++ main.ts
@@ -1,22 +1,19 @@
-import blog, { ga, redirects } from "https://deno.land/x/blog@0.4.2/blog.tsx";
+import blog from "https://deno.land/x/blog@0.4.2/blog.tsx";
+import "https://esm.sh/prismjs@1.27.0/components/prism-go";
 
 blog({
-  title: "My Blog",
-  description: "This is my new blog.",
-  avatar: "https://deno-avatar.deno.dev/avatar/blog.svg",
+  author: "Musa Barighzaai",
+  title: "Musa Barighzaai",
+  description:
+    "Software Engineer from Toronto, Canada. This site is dedicated to my thoughts, observations, and learnings about engineering, and productivity.",
+  avatar: "/assets/avatar.webp",
   avatarClass: "rounded-full",
-  author: "An author",
-
-  // middlewares: [
-
-    // If you want to set up Google Analytics, paste your GA key here.
-    // ga("UA-XXXXXXXX-X"),
-
-    // If you want to provide some redirections, you can specify them here,
-    // pathname specified in a key will redirect to pathname in the value.
-    // redirects({
-    //  "/hello_world.html": "/hello_world",
-    // }),
-
-  // ]
+  links: [
+    { title: "Email", url: "mailto:musab@icloud.com" },
+    { title: "GitHub", url: "https://github.com/musab" },
+    { title: "Twitter", url: "https://twitter.com/barighzaai" },
+  ],
+  lang: "en",
+  dateStyle: "long", // localised format based on https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat
+  favicon: "/assets/favicon.ico",
 });
