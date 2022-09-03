2d5b90e85ada5e8e4a74739ac85a55371cee6ee9
Musa Barighzaai <mbarighzaai@icloud.com>
2022-09-03 16:11:36 -0400

    update date style

--- main.ts
+++ main.ts
@@ -14,6 +14,7 @@ blog({
     { title: "Twitter", url: "https://twitter.com/barighzaai" },
   ],
   lang: "en",
-  dateStyle: "long", // localised format based on https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat
-  favicon: "/assets/favicon.ico",
+  dateStyle: "medium", // localised format based on https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat
+  favicon: "/assets/favicon.png",
+  theme: "dark",
 });
