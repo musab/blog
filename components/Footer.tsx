360931b2597aca7b4f64669d76df5526fe70954b
Musa Barighzaai <34455928+musab@users.noreply.github.com>
2023-01-03 12:49:59 -0500

    Footer → 2023

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ export const Footer = () => {
   return (
     <footer class="mt-20 pb-16 lt-sm:pb-8 lt-sm:mt-16">
       <p class="flex justify-between items-center gap-2.5 text-gray-400/800 dark:text-gray-500/800 text-sm">
-        <span>© 2022 - Made with ❤ by Musa Barighzaai</span>
+        <span>© 2023 - Made with ❤ by Musa Barighzaai</span>
         <a
           href="/feed"
           class="inline-flex items-center gap-1 hover:text-gray-800 dark:hover:text-gray-200 transition-colors"
